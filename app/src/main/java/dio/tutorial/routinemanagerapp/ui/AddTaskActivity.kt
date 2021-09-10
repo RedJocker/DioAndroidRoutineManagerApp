a80359068c8e81a19a2823c74ce6e8454d090255
RedJocker85 <mauricio.guaruja@gmail.com>
2021-09-09 22:51:10 -0300

    timepicker

--- app/src/main/java/dio/tutorial/routinemanagerapp/ui/AddTaskActivity.kt
+++ app/src/main/java/dio/tutorial/routinemanagerapp/ui/AddTaskActivity.kt
@@ -3,6 +3,8 @@ package dio.tutorial.routinemanagerapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import dio.tutorial.routinemanagerapp.databinding.ActivityAddTaskBinding
 import dio.tutorial.routinemanagerapp.extensions.format
 import java.util.*
@@ -30,6 +32,20 @@ class AddTaskActivity : AppCompatActivity() {
                 val offset = timeZone.getOffset(Date().time) * -1
 
                 binding.tilDate.editText?.setText(Date(it + offset).format())
+            }
+
+            binding.tilHour.editText?.setOnClickListener {
+                val timerPicker = MaterialTimePicker.Builder()
+                    .setTimeFormat(TimeFormat.CLOCK_24H)
+                    .build()
+
+                timerPicker.addOnPositiveButtonClickListener {
+                    val hour = "%02d".format(timerPicker.hour)
+                    val minutes = "%02d".format(timerPicker.minute)
+                    binding.tilHour.editText?.setText("$hour:$minutes")
+                }
+
+                timerPicker.show(supportFragmentManager, "TIME_PICKER_TAG")
             }
         }
     }
